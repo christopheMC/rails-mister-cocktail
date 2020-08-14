17ca08b6b1ede60da96ce1b291b27176c90aad11
christophe mohammed-cherif <krisfromtama@gmail.com>
2020-08-14 12:57:11 +0200

    instaal

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,9 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_one_attached :photo
 
   validates :name, uniqueness: true
   validates :name, presence: true
+  validates :photo, presence: true
 end
